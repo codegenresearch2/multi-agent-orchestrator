0da273537cc4718fbf3474f1ab33aad60d6f2ab9
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-08 11:46:06 +0000

    Reset to 34247642ab52dc58683cd3c58fe1048d62553614 and made changes for LLM Round 1

--- examples/movie-production/movie-production-demo.py
+++ examples/movie-production/movie-production-demo.py
@@ -1,108 +1,55 @@
-import uuid
-import asyncio
 import streamlit as st
 import os
-from  search_web import tool_handler
-from tool import Tool
+import uuid
+import asyncio
 from multi_agent_orchestrator.orchestrator import MultiAgentOrchestrator, OrchestratorConfig
-from multi_agent_orchestrator.agents import (
-    AgentResponse,
-    BedrockLLMAgent,
-    BedrockLLMAgentOptions
-)
+from multi_agent_orchestrator.agents import (AnthropicAgent, AnthropicAgentOptions, AgentResponse)
 from multi_agent_orchestrator.types import ConversationMessage
 from multi_agent_orchestrator.classifiers import ClassifierResult
-from supervisor_agent import SupervisorAgent, SupervisorAgentOptions
+from supervisor import SupervisorMode, SupervisorModeOptions
 
 # Set up the Streamlit app
 st.title("AI Movie Production Demo 🎬")
 st.caption("Bring your movie ideas to life with the teams of script writing and casting AI agents")
 
+# Get Anthropic API key from user
+anthropic_api_key = st.text_input("Enter Anthropic API Key to access Claude Sonnet 3.5", type="password", value=os.getenv('ANTHROPIC_API_KEY', None))
 
-search_web_tool = Tool(name='search_web',
-                          description='Search Web for information',
-                          properties={
-                              'query': {
-                                  'type': 'string',
-                                  'description': 'The search query'
-                              }
-                          },
-                          required=['query'])
-
-script_writer_agent = BedrockLLMAgent(BedrockLLMAgentOptions(
-    model_id='us.anthropic.claude-3-sonnet-20240229-v1:0',
+script_writer_agent = AnthropicAgent(AnthropicAgentOptions(
+    api_key=os.getenv('ANTHROPIC_API_KEY', None),
     name="ScriptWriterAgent",
-    description="""\
-You are an expert screenplay writer. Given a movie idea and genre,
-develop a compelling script outline with character descriptions and key plot points.
-
-Your tasks consist of:
-1. Write a script outline with 3-5 main characters and key plot points
-2. Outline the three-act structure and suggest 2-3 twists.
-3. Ensure the script aligns with the specified genre and target audience
-"""))
-
-casting_director_agent = BedrockLLMAgent(BedrockLLMAgentOptions(
-    model_id='anthropic.claude-3-haiku-20240307-v1:0',
-    name="CastingDirectorAgent",
-    description="""\
-You are a talented casting director. Given a script outline and character descriptions,\
-suggest suitable actors for the main roles, considering their past performances and current availability.
-
-Your tasks consist of:
-1. Suggest 1-2 actors for each main role.
-2. Check actors' current status using search_web tool
-3. Provide a brief explanation for each casting suggestion.
-4. Consider diversity and representation in your casting choices.
-5. Provide a final response with all the actors you suggest for the main roles
-""",
-
-tool_config={
-    'tool': [search_web_tool.to_bedrock_format()],
-    'toolMaxRecursions': 20,
-    'useToolHandler': tool_handler
-    },
-    save_chat=False
+    description="""\nYou are an expert screenplay writer. Given a movie idea and genre,
+develop a compelling script outline with character descriptions and key plot points."""
 ))
 
-movie_producer_supervisor = BedrockLLMAgent(BedrockLLMAgentOptions(
-    model_id='us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+casting_director_agent = AnthropicAgent(AnthropicAgentOptions(
+    api_key=os.getenv('ANTHROPIC_API_KEY', None),
+    name="CastingDirectorAgent",
+    description="""\nYou are a talented casting director. Given a script outline and character descriptions,\
+suggest suitable actors for the main roles, considering their past performances and current availability."""
+))
+
+movie_producer_supervisor = AnthropicAgent(AnthropicAgentOptions(
+    api_key=os.getenv('ANTHROPIC_API_KEY', None),
     name='MovieProducerAgent',
     description="""
-Experienced movie producer overseeing script and casting.
-
-Your tasks consist of:
-1. Ask ScriptWriter Agent for a script outline based on the movie idea.
-2. Pass the outline to CastingDirectorAgent for casting suggestions.
-3. Summarize the script outline and casting suggestions.
-4. Provide a concise movie concept overview.
-5. Make sure to respond with a markdown format without mentioning it.
-""",
+Experienced movie producer overseeing script and casting."""
 ))
 
-supervisor = SupervisorAgent(SupervisorAgentOptions(
+supervisor = SupervisorMode(SupervisorModeOptions(
     supervisor=movie_producer_supervisor,
     team=[script_writer_agent, casting_director_agent],
     trace=True
 ))
 
-
-
 async def handle_request(_orchestrator: MultiAgentOrchestrator, _user_input:str, _user_id:str, _session_id:str):
     classifier_result=ClassifierResult(selected_agent=supervisor, confidence=1.0)
-
     response:AgentResponse = await _orchestrator.agent_process_request(_user_input, _user_id, _session_id, classifier_result)
-
-    # Print metadata
-    print("\nMetadata:")
-    print(f"Selected Agent: {response.metadata.agent_name}")
     if isinstance(response, AgentResponse) and response.streaming is False:
-        # Handle regular response
         if isinstance(response.output, str):
-            return (response.output)
+            return response.output
         elif isinstance(response.output, ConversationMessage):
-                return (response.output.content[0].get('text'))
-
+            return response.output.content[0].get('text')
 
 # Initialize the orchestrator with some options
 orchestrator = MultiAgentOrchestrator(options=OrchestratorConfig(
