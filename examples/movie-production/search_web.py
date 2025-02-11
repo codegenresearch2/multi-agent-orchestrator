2192c8869353c5e0de93d41f544e385b83a2e82a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:58:58 +0000

    Reset to 34247642ab52dc58683cd3c58fe1048d62553614 and made changes for LLM Round 1

--- examples/movie-production/search_web.py
+++ examples/movie-production/search_web.py
@@ -11,35 +11,7 @@ except ImportError:
     class BedrockLLMAgent:
         pass
 
-async def tool_handler(response: Any, conversation: list[dict[str, Any]],) -> Any:
-    if not response.content:
-        raise ValueError("No content blocks in response")
-
-    tool_results = []
-    content_blocks = response.content
-
-    for block in content_blocks:
-        tool_use_block = block.get("toolUse") if "toolUse" in block else None
-        if not tool_use_block:
-            continue
-
-        tool_name = tool_use_block.get("name")
-        tool_id = tool_use_block.get("toolUseId")
-        input_data = tool_use_block.get("input")
-
-        if tool_name == "search_web":
-            result = await search_web(input_data.get('query'))
-        else:
-            result = f"Unknown tool use name: {tool_name}"
-            Logger.error(result)
-
-        tool_result = ToolResult(tool_id, result)
-        formatted_result = tool_result.to_bedrock_format()
-        tool_results.append(formatted_result)
-
-    return tool_results
-
-async def search_web(query: str, num_results: int = 2) -> str:
+def search_web(query: str, num_results: int = 2) -> str:
     """
     Search Web using the DuckDuckGo. Returns the search results.
 
@@ -63,3 +35,34 @@ async def search_web(query: str, num_results: int = 2) -> str:
     except Exception as e:
         Logger.error(f"Error searching for the query {query}: {e}")
         return f"Error searching for the query {query}: {e}"
+
+def tool_handler(response: Any, conversation: list[dict[str, Any]],) -> ConversationMessage:
+    if not response.content:
+        raise ValueError("No content blocks in response")
+
+    tool_results = []
+    content_blocks = response.content
+
+    for block in content_blocks:
+        tool_use_block = block.get("toolUse") if "toolUse" in block else None
+        if not tool_use_block:
+            continue
+
+        tool_name = tool_use_block.get("name")
+        tool_id = tool_use_block.get("toolUseId")
+        input_data = tool_use_block.get("input")
+
+        if tool_name == "search_web":
+            result = search_web(input_data.get('query'))
+        else:
+            result = f"Unknown tool use name: {tool_name}"
+            Logger.error(result)
+
+        tool_result = ToolResult(tool_id, result)
+        formatted_result = tool_result.to_bedrock_format()
+        tool_results.append(formatted_result)
+
+    return ConversationMessage(
+        role=ParticipantRole.USER.value,
+        content=tool_results
+    )
